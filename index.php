29983bbb36a024a5da0cc372b7f7c2893daa1e45
lesha <lesha@10.0.0.9>
2020-12-06 20:05:34 -0600

    php login demo

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>    
+<head>    
+    <title>K Antoinnette Transitions</title>    
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<!-- Add icon library -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<style>
+            body, html {
+            height: 100%;
+            margin: 0;}
+
+        .bg {
+        /* The image used */
+        background-image: url("bedroom.jpg");
+
+        /* Full height */
+        height: 100%; 
+
+        /* Center and scale the image nicely */
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        }
+        </style>
+</head>    
+
+<body oncontextmenu = "return false">
+
+ <div class="topnav">
+<?php include 'menu.php';?>
+</div> 
+
+<div class="bg"></div>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+  echo'
+  <a href="index.php">Home</a>
+  <a href="Portfolio.html">Portfolio</a>
+  <a href="About.html">About</a>
+  <a href="Tips.html">Tips and Tricks</a>
+  <a href="login.php" >Login</a>
+  <a href="#" class="fa fa-linkedin"></a>
+  <a href="#" class="fa fa-youtube"></a>
+  <a href="http://www.facebook.com/kantoinettetransitions/?ref=page_internal" class="fa fa-facebook"></a>
+  <a href="#" class="fa fa-twitter"></a>';
+  
+    ?>
